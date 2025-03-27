8119ead73c6dccb06e0dc23756556867406c5a90
Brostouch KOK <brostouchkokit1007@gmail.com>
2025-03-27 15:56:30 +0700

    dark mode feature and dropdown link menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { menuLinks } from "../../assets/constant/menuLinks";
+import { menuLinks } from "../../constant/menuLinks";
 import { IoMdSearch } from "react-icons/io";
+import { FaCaretDown, FaShoppingCart } from "react-icons/fa";
+import DarkMode from "./DarkMode";
+import { dropDownLinks } from "../../constant/dropDownLinks";
 
 const Navbar = () => {
   return (
@@ -26,18 +29,49 @@ const Navbar = () => {
                   <li key={index}>
                     <a
                       href={data.link}
-                      className="inline-block px-4 text-gray-500 dark:text-white
+                      className="inline-block px-4 text-gray-500 dark:text-gray-500 dark:hover:text-white
                                 hover:text-black duration-200 font-semibold"
                     >
                       {data.name}
                     </a>
                   </li>
                 ))}
+                {/* dropDown */}
+                <li className="relative cursor-pointer group">
+                  <a
+                    href="#"
+                    className="flex items-center gap-[2px] font-semibold
+                    text-gray-500 dark:hover:text-white py-2"
+                  >
+                    Quick Links
+                    <span>
+                      <FaCaretDown className="group-hover:rotate-180 duration-300" />
+                    </span>
+                  </a>
+                  <div
+                    className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md
+                    bg-white shadow-md p-2 dark:bg-gray-900 dark:text-white"
+                  >
+                    <ul className="space-y-2">
+                      {dropDownLinks.map((data, index) => (
+                        <li key={index}>
+                          <a
+                            href={data.link}
+                            className="inline-block text-gray-500
+                            dark:hover:text-white duration-200 hover:bg-primary/20 w-full rounded-md py-1 px-2"
+                          >
+                            {data.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
           {/* navbar right section */}
-          <div className="felx justify-center items-center gap-4">
+          <div className="flex justify-between items-center gap-4">
             {/* search bar section */}
             <div className="relative group hidden sm:block">
               <input type="text" placeholder="Search" className="search-bar" />
@@ -48,8 +82,19 @@ const Navbar = () => {
               />
             </div>
             {/* order button section */}
+            <button className="relative p-3">
+              <FaShoppingCart className="text-xl text-gray-600 dark:text-gray-400" />
+              <div
+                className="w-4 h-4 bg-red-500 text-white rounded-full
+                absolute top-0 right-0 flex justify-center items-center text-xs"
+              >
+                4
+              </div>
+            </button>
             {/* dark mode section */}
-            <div></div>
+            <div>
+              <DarkMode />
+            </div>
           </div>
         </div>
       </div>
