2ac0a3ae0b3c45b6bd616dd5bd43714604bd64ed
Brostouch KOK <brostouchkokit1007@gmail.com>
2025-03-27 21:39:35 +0700

    category section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
+import Category from './components/category/Category'
+import Category2 from './components/category/Category2'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Category/>
+      <Category2/>
+      
     </div>
   )
 }
